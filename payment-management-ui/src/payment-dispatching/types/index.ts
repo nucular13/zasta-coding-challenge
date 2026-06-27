36e559206903130052eb73f4894c97ed586ffbad
nucular13 <pr0lli@gmx.de>
2026-06-27 20:54:15 +0200

    Set up payment management types file in separate folder.

---
+++ payment-management-ui/src/payment-dispatching/types/index.ts
@@ -0,0 +1,20 @@
+export type Customer = {
+    id: number;
+    name: string;
+}
+
+export type Invoice = {
+    id: number;
+    amount: number;
+    invoiceNumber: string;
+    customer: Customer;
+}
+
+export type Payment = {
+    id: number;
+    amount: number;
+    senderName: string;
+    referenceNumber: string;
+}
+
+
